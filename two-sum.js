d5873022fbb1622702a7681c8685ae7047d220fa
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-13 18:54:41 -0500

    two-sum done

--- two-sum.js
+++ two-sum.js
@@ -1,1 +1,14 @@
-// new file is here
+// Given nums = [2, 7, 11, 15], target = 9
+// Because nums[0] + nums[1] = 2 + 7 = 9
+// return [0, 1]
+
+var twoSum = function(nums, target) {
+  let sets = {};
+  for (var i = 0; i < nums.length; i++) {
+      if (sets[nums[i]] >= 0) {
+          return [sets[nums[i]], i]
+      } else {
+          sets[target-nums[i]] = i
+      }
+  }
+};
