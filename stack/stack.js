5b04def7286c6261e7b6c53670e466d355a9ee9b
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-16 13:08:18 -0500

    stack practice doen

---
+++ stack/stack.js
@@ -0,0 +1,60 @@
+// function: push, pop, peek, length
+
+
+// ex1) Palindrome
+var letters = []
+var word = "racecar"
+var rword = ""
+
+for (var i = 0; i < word.length; i++) {
+  letters.push(word[i])
+}
+
+for (var i = 0; i < word.length; i++) {
+  rword += letters.pop()
+}
+
+if (rword === word) {
+  return true
+} else { return false }
+
+
+// creat a stack
+var Stack = function() {
+  this.count = 0
+  this.storage = {}
+
+  this.push = function(value) {
+    this.storage[this.count] = value
+    this.count++
+  }
+
+  this.pop = function() {
+    if (this.count === 0) {
+      return undefined
+    }
+
+    this.count--;
+    var result = this.storage[this.count]
+    delete this.storage[this.count]
+    return result
+  }
+
+  this.size = function() {
+    return this.count
+  }
+
+  // peek doesn't remove the item
+  this.peek = function(value) {
+    return this.storage[this.count-1]
+  }
+}
+
+var myStack = new Stack()
+
+myStack.push(1)
+myStack.push(2)
+console.log(myStack.peek()) // 2
+console.log(myStack.pop()) // 2
+console.log(myStack.peek()) // 1
+
