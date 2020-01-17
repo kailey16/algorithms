8f30dd7c158ef7222060ebd982b13678bfa5068c
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-17 12:42:57 -0500

    group anagrams done

---
+++ Arrays-and-strings/group-anagrams.js
@@ -0,0 +1,30 @@
+// Given an array of strings, group anagrams together.
+
+// Example:
+// Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+// Output:
+// [
+//   ["ate","eat","tea"],
+//   ["nat","tan"],
+//   ["bat"]
+// ]
+
+// go through each string and sort it and group together
+
+var groupAnagrams = function(strs) {
+  let groups = {};
+  for (let i = 0; i < strs.length; i++) {
+      let sortedStr = strs[i].split("").sort().join("")
+      if (groups[sortedStr]) {
+          groups[sortedStr].push(strs[i])
+      } else {
+          groups[sortedStr] = [strs[i]]
+      }
+  }
+  
+  let groupsArray = [];
+  for (let key in groups) {
+      groupsArray.push(groups[key])
+  }
+  return groupsArray
+};
--- Arrays-and-strings/is-anagram.js
+++ Arrays-and-strings/is-anagram.js
@@ -1,3 +1,4 @@
+// Solution O(n)
 var isAnagram = function(s, t) {
     
   if (s.length !== t.length) {
