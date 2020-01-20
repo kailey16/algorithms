7cc7ca473276a50207464fed8ace3c41b0c450b8
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-19 22:41:27 -0500

    add two numbers done

---
+++ Linked-list/AddTwoNumbers.js
@@ -0,0 +1,29 @@
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+// Example:
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+// Explanation: 342 + 465 = 807.
+
+var addTwoNumbers = function(l1, l2) {
+  let head = new ListNode(0)
+  let current = head
+  let carry = 0
+  let sum = 0
+  while (l1 || l2) {
+      let x = l1 ? l1.val : 0
+      let y = l2 ? l2.val : 0
+      sum = x + y + carry
+      sum > 9 ? carry = 1 : carry = 0
+      current.next = new ListNode(sum % 10)
+      current = current.next
+      l1 ? l1 = l1.next : null
+      l2 ? l2 = l2.next : null
+  }
+  if (carry > 0) {
+      current.next = new ListNode(carry)
+  }
+  return head.next
+};
