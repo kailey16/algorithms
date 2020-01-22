835e349c74d93b4a6580d2558543218133f576dc
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-22 14:02:43 -0500

    Best time to buy and sell stock, array solution 2

--- Arrays-and-strings/BestTimeToBuyAndSellStock.js
+++ Arrays-and-strings/BestTimeToBuyAndSellStock.js
@@ -26,3 +26,14 @@ var maxProfit = function(prices) {
   return profit
 };
 
+// Solution O(n)
+var maxProfit = function(prices) {
+  let min = prices[0]
+  let profit = 0
+  
+  for (let i = 0; i < prices.length; i++) {
+      min = Math.min(min, prices[i])
+      profit = Math.max(profit, prices[i] - min)
+  }
+  return profit
+};
