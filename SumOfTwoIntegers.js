54fabd6bcf627aa916d7a3b350922edfd28947cc
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-22 17:45:07 -0500

    sum of two integers done

---
+++ SumOfTwoIntegers.js
@@ -0,0 +1,18 @@
+// Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+var getSum = function(a, b) {
+    let arr = []
+    arr.push(a)
+    arr.push(b)
+    let result = arr.reduce((memo, num) => memo += num, 0)
+    return result
+};
+
+var getSum = function(a, b) {
+  let arr = [a, b]
+  let total = 0
+  for (let i = 0; i < arr.length; i++) {
+      total += arr[i]
+  }
+  return total
+}
