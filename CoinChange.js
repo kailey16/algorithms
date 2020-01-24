6a2bdcfa52a9553eb5a1993209cc6b07f48bd063
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-24 10:44:06 -0500

    coin change bottom up solution added

--- CoinChange.js
+++ CoinChange.js
@@ -6,39 +6,23 @@
 // Explanation: 11 = 5 + 5 + 1
 
 
-// NOT A WORKING SOLUTION!!!!
-
-// coins = [1, 2, 5], amount 11
+// bottom up approach
+// Solution O(A*n), space complexity O(A)  --  A: amount
 var coinChange = function(coins, amount) {
-  coins.sort((a,b) => b-a) // [5, 2, 1]
-  
-  if (amount === 0) { return 0 }
-  if (coins[coins.length-1] > amount) { return -1 }
-  if (coins.length === 1) {
-      if (amount % coins[0] === 0) { return amount / coins[0] }
-      return coins[0] === amount ? 1 : -1
-  }
-  
-  // use the biggest coin as many as possible
-  // and call recursive with the left over
-  let count;
-  for (let i = 0; i < coins.length; i++) {
-      let totalCount = 0
-      const numOfCoin = Math.floor(amount / coins[i])
-      totalCount += numOfCoin
-      
-      const leftCoins = coins.splice(1)
-      const leftAmount = amount - coins[i]*numOfCoin
-      const recurCount = coinChange(leftCoins, leftAmount)
-      if (recurCount >= 0) {
-          totalCount += recurCount
-      }
-      
-      if (i === 0) {count = totalCount}
-      if (totalCount < count) {
-          count = totalCount
-      }
-  }
-  
-  return count
-};
+    const max = amount + 1
+    let dp = new Array(amount+1) 
+    // make a new array with length amoutn + 1, this will store the answer of the sub problems
+    dp.fill(max)
+    dp[0] = 0
+    
+    for (let i = 1; i <= amount; i++) {
+        // for loop for each coin we are given
+        for (let j = 0; j < coins.length; j++) {
+            // if the coin is less than or equal to the sub problem amount
+            if (coins[j] <= i) {
+                dp[i] = Math.min(dp[i], dp[i-coins[j]]+1)
+            }
+        }
+    }
+    return dp[amount] > amount ? -1 : dp[amount]
+}
