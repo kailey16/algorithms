9729009cd547f62011d19bb88c399802054f9369
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-02-11 18:43:06 -0500

    palindromic substrings done

--- String/PalindromicSubstrings.js
+++ String/PalindromicSubstrings.js
@@ -11,3 +11,40 @@
 // Output: 6
 // Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 
+// Solution
+var countSubstrings = function(s) {
+  let count = 0
+  for (let i = 0; i < s.length; i++) {
+      for (let j = i + 1; j < s.length + 1; j++) {
+          let subStr = s.substring(i,j)
+          if (palindrome(subStr)) {
+              count++;
+          }
+      }
+  }
+  return count
+};
+
+var palindrome = function(string) {
+  return string === string.split("").reverse().join("")
+}
+
+
+// Solution O(n)
+var countSubstrings = function(s) {
+  const checkPalindrome = (start, end, s) => {
+      while (start >= 0 && end < s.length && s[start] === s[end]) {
+          count++
+          start--
+          end++
+      }
+  }
+  
+  let count = 0
+  for (let i = 0; i < s.length; i++) {
+      checkPalindrome(i, i, s)        
+      checkPalindrome(i, i+1, s)  
+  }
+  
+return count
+}
