1fda95b0a90a1871aa79c969fdffc2ca2d160bb4
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-02-18 18:45:33 -0500

    is prime, largest prime factor done

---
+++ Other/LargestPrimeFactor.js
@@ -0,0 +1,13 @@
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
+function largestPrimeFactor(num) {
+  while (number > 1) {
+    if (number % divisor === 0) { 
+      number /= divisor;
+    } else {
+      divisor++;
+    }
+  }
+  return divisor
+}
--- Other/PrimesInArray.js
+++ Other/PrimesInArray.js
@@ -19,16 +19,3 @@ function primesInArray(arr) {
   }
 }
 
-
-function isPrime (n) {
-    if (n < 2) return false;
-
-    var q = Math.floor(Math.sqrt(n));
-    for (var i = 2; i <= q; i++) {
-        if (n % i == 0) {
-          return false;
-        }
-    }
-    return true;
-}
-
