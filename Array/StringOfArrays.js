677146603f5fee0ae86e42a74d32d49b7ae7faa5
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-02-28 17:39:50 -0500

    JSON.parse() explanation added

--- Array/StringOfArrays.js
+++ Array/StringOfArrays.js
@@ -3,3 +3,9 @@
 // return [[-5, 5, 5], [-6, 15, 15], [7, 13, 12]]
 
 var strToArr = string => JSON.parse('[' + string + ']')
+
+
+// JSON.parse()
+// A common use of JSON is to exchange data to/from a web server
+// When receiving data from a web server, the data is always a string
+// Parse the data with JSON.parse(), and the data becomes a JavaScript object
